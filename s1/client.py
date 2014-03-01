7bc7f978f468acce7307d044462561fe066c549b
hfeng.air <harrifeng@gmail.com>
2014-03-01 20:19:55 +0800

    can run now

--- s1/client.py
+++ s1/client.py
@@ -6,7 +6,9 @@ def main():
     PORT = 50007
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST,PORT))
-    s.sendall("Hello World")
+
+    sin = raw_input("->")
+    s.sendall(sin)
     data = s.recv(1024)
     s.close()
     print "Received", repr(data)
--- s1/server.py
+++ s1/server.py
@@ -7,7 +7,7 @@ def main():
     PORT = 50007
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
     s.bind((HOST, PORT))
-    s.listen()
+    s.listen(1)
 
     conn, addr = s.accept()
     print "Connected by", addr
