0d34f62caf8964af5c14dac5b8cace70947025d0
harrifeng <harrifeng@gmail.com>
2014-03-01 20:40:04 +0800

    test SO_REUSEADDR

---
+++ s2/server.py
@@ -0,0 +1,22 @@
+import socket
+
+__author__ = 'hfeng'
+
+def main():
+    HOST = '0.0.0.0'
+    PORT = 50007
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    print "Connected by", addr
+    while 1:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+    conn.close()
+
+if __name__ == '__main__':
+    main()
+
